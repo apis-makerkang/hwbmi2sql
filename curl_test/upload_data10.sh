1d5f9f8ab9813710198a08a9e28a4ca96a7e3796
paulchainkang <sabg@paul-kang.com>
2023-07-11 15:31:07 +0800

    wip

--- curl_test/upload_data10.sh
+++ curl_test/upload_data10.sh
@@ -1,2 +1,2 @@
 # change 192.168.2.56 to the ip of the corresponding Mapleboard
-curl -H "Content-Type: application/json" --data @upload_data10.json "http://192.168.2.56/status?XAPIKEY=xG0y3ziAPN"
+curl -H "Content-Type: application/json" --data @upload_data10.json "http://192.168.1.100/status?XAPIKEY=xG0y3ziAPN"
